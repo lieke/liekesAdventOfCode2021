c4c43ac34b06f8b1c1872885c59e0fd5ef526e94
Lieke van der Poel <lieke@MacBook-Pro-van-Lieke.local>
2021-12-03 14:31:41 +0100

    added solution for the first part of day three

--- daythree/src/test/scala/Test1.scala
+++ daythree/src/test/scala/Test1.scala
@@ -28,14 +28,33 @@ class Test1:
     assertEquals(7, amountOf1s)
     assertEquals(5, amountOf0s)
 
-  @Test def findBinaryEpsilon(): Unit = 
+  @Test def findEpsilonAndGamma(): Unit = 
     val totalAmountOfBinaryNumbers = numberList.length
-    val gammaString: String = createAListWithAllTheValuesAddedUp(numberList)
-      .map(amountOf1s => if(amountOf1s > totalAmountOfBinaryNumbers - amountOf1s) "1" else "0")
-      .mkString("")
-    assertEquals("10110", gammaString)
+    val gammaList: List[Int] = createAListWithAllTheValuesAddedUp(numberList)
+      .map(amountOf1s => if(amountOf1s > totalAmountOfBinaryNumbers - amountOf1s) 1 else 0)
+    val epsilonList: List[Int] = gammaList
+      .map(element => if(element == 0) 1 else 0)
+    val gamma:Int = Integer.parseInt(gammaList.mkString(""), 2)
+    val epsilon:Int = Integer.parseInt(epsilonList.mkString(""), 2)
+    assertEquals("10110", gammaList.mkString(""))
+    assertEquals("01001", epsilonList.mkString(""))
+    assertEquals(22, gamma)
+    assertEquals(9, epsilon)
   
+  @Test def findThePowerConsumption(): Unit = 
+    val powerConsumption:Int = findPowerConsumption(numberList)
+    assertEquals(198, powerConsumption)
 
+def findPowerConsumption(binaryNumbersList: List[String]): Int = 
+  val totalAmountOfBinaryNumbers = binaryNumbersList.length
+  val gammaList: List[Int] = createAListWithAllTheValuesAddedUp(binaryNumbersList)
+    .map(amountOf1s => if(amountOf1s > totalAmountOfBinaryNumbers - amountOf1s) 1 else 0)
+  val epsilonList: List[Int] = gammaList
+    .map(element => if(element == 0) 1 else 0)
+  createDecimalNumberFromList(gammaList) * createDecimalNumberFromList(epsilonList)
+
+def createDecimalNumberFromList(binaryAsList: List[Int]): Int = 
+  Integer.parseInt(binaryAsList.mkString(""), 2)
 
 def createAParsedBinaryNumberFromString(numberString: String): List[Int] = 
   numberString.map(char => if(char == '0') 0 else 1).toList
